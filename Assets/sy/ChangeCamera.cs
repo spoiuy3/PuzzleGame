d78adc53b8dbef4db5c749f03faf24c26340426c
spoiuy3 <kuniyeoni@gmail.com>
2023-11-12 04:41:45 +0900

    Revert "forest map"
    This reverts commit 4fbef54bf3310aa5865fc8683bd757217ea70470.

--- Assets/sy/ChangeCamera.cs
+++ Assets/sy/ChangeCamera.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 
 public class ChangeCamera : MonoBehaviour
 {
-    public Camera mainCamera;
     public CinemachineVirtualCamera c1;
     public CinemachineVirtualCamera c2;
+
     void SwitchCamera()
     {
-        if (c1.Priority>c2.Priority)  // 현재 Orthographic 투영인 경우
-        {
-            c1.Priority = 0;  // 2D 카메라 비활성화
-            c2.Priority = 1;  // 2.5D 카메라 활성화
-        }
-        else
-        {
-            c1.Priority = 1;  // 2D 카메라 비활성화
-            c2.Priority = 0;  // 2.5D 카메라 활성화
-        }
+        c1.Priority = 0;  // 2D 카메라 비활성화
+        c2.Priority = 1;  // 2.5D 카메라 활성화
     }
 
+    void SwitchCamera1()
+    {
+        c1.Priority = 1;  // 2D 카메라 비활성화
+        c2.Priority = 0;  // 2.5D 카메라 활성화
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -32,6 +29,10 @@ public class ChangeCamera : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.V))  // V 키를 눌렀을 때
         {
             SwitchCamera();  // 카메라 전환 함수 호출
+        }
+        if (Input.GetKeyDown(KeyCode.K))  // V 키를 눌렀을 때
+        {
+            SwitchCamera1();  // 카메라 전환 함수 호출
         }
     }
 }
