43c7d99fbaf8132a1cbdc3769932f54d4a9fb6cd
jjuni0919 <60464302+jjuni0919@users.noreply.github.com>
2023-11-18 04:41:15 +0900

    a

--- Assets/sy/movescript1.cs
+++ Assets/sy/movescript1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static UnityEditor.Rendering.FilterWindow;
+
 
 public class movescript1 : MonoBehaviour
 {
